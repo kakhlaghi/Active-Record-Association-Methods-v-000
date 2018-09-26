08764c566fdc820857237a7e36c4148cfed9053d
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-26 00:51:28 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.all.name
+    self.artists.all.collect do |name| name end
   end
 end
